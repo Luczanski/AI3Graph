bdb162cd0301982794b4138a9b7a571772b22b33
nrtqq <93160388+Luczanski@users.noreply.github.com>
2024-05-10 15:15:49 +0200

    navMesh

--- Assets/Scripts/BotFollow.cs
+++ Assets/Scripts/BotFollow.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor.AI;
-    
+using UnityEngine.AI;
+
 
 public class BotFollow : MonoBehaviour
-{
-    
-    void Start()
-    {
-        
-    }
+{ 
+    [SerializeField] public NavMeshAgent bot;
+    [SerializeField] public Transform Player; 
     void Update()
     {
-        
+        bot.SetDestination(Player.position);
     }
 }
